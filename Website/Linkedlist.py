cd902eb5f1652c0350f3ee7e3b297b72ccb0c90d
c programming <2403A52165@sru.edu.in>
2026-06-22 15:02:22 +0530

    Third commit

--- Website/Linkedlist.py
+++ Website/Linkedlist.py
@@ -2,9 +2,11 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
     def add_end(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -14,3 +16,18 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
+
+    def display(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+ll = LinkedList()  
+ll.add_end(100)
+ll.add_end(150)
+ll.add_end(350)
+ll.add_end(400)
+ll.add_end(450)
+ll.display()
